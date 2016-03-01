1927d5519d918334c17805239ad1489ab0b05385
baiiu <baiiu@foxmail.com>
2016-03-01 22:28:27 +0800

    modify the json data

--- app/src/main/java/com/baiiu/krtvdemo/pojo/KrTVData.java
+++ app/src/main/java/com/baiiu/krtvdemo/pojo/KrTVData.java
@@ -1,16 +1,17 @@
 package com.baiiu.krtvdemo.pojo;
 
-import java.util.List;
-
 /**
  * auther: baiiu
- * time: 16/2/27 27 11:32
+ * time: 16/2/27 27 11:35
  * description:
  */
 public class KrTVData {
-    public int page;
-    public int totalCount;
-    public int pageSize;
-    public int totalPagest;
-    public List<TV> data;
+    public String title;
+    public String duration;
+    public String videoSource;
+    public long publishTime;
+    public String youkuUrl;
+    public String featureImg;
+    public String durationLong;
+
 }
